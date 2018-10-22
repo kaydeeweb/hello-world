d5308f3b601f7e5e260efa8de283abce4308eaf8
Kelly Drewett <design@kaydee.net>
2018-10-22 09:34:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,73 +1,7 @@
-<!DOCTYPE html>
+<h1>Hello, World!</h1>
 
-<html lang="en">
+<h2>This is a pull request</h2>
 
-  <head>
+<p>Kaydee Web enters into the world of programming...<p>
 
-   <!-- Header -->
-   <?php include 'head.inc.php';?> 
-
-    <title>Hello, World</title>
-
-    <meta name="description" content=""/>
-
-    <!-- TODO <meta name="date" content="20148-03-14" scheme="YYYY-MM-DD"> -->
-
-    <!-- OpenGraph -->
-
-    <?php include 'og-common.inc.php';?> 
-
-    <meta property="og:title" content=""/>
-
-    <meta property="og:description" content=""/>
-
-    <meta property="og:image" content=""/>
-
-    <link rel="canonical" href=""/>
-
-  </head> 
-
-
-<body itemscope itemtype="http://schema.org/LocalBusiness"> 
-
-  <a href="#content" class="sr-only sr-only-focusable">Skip to main content</a>
-
-
-      <div class="container"> <!-- Container -->
-
-        <!-- Navigation -->
-
-        <?php include 'nav.inc.php';?> 
-
-        <!-- Start of page -->
-
-
-          <main id="content" itemscope itemtype ="http://schema.org/CreativeWork">
-
-                
-                <section class="row">
-
-                            <div class="col-6">Hello, World</div>
-
-                            <div class="col-6">Hello, Me</div>
-
-                </section>
-
-
-
-          </main>
-
-
-
-         <!-- Footer -->
-
-        <?php include 'footer.inc.php';?>
-
-
-    </div>  <!-- Full width container -->
-
-
-  </body>
-
-</html>
 
